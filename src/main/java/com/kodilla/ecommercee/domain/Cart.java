74e807f27bc9d25da744ffc1043254234a4ab769
PawelMurawski <pawelt.murawski@gmail.com>
2019-08-29 17:11:53 +0200

    JDP190803-14 - Cart entity beta

---
+++ src/main/java/com/kodilla/ecommercee/domain/Cart.java
@@ -0,0 +1,68 @@
+package com.kodilla.ecommercee.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.math.BigDecimal;
+
+@Entity(name = "carts")
+@Table
+@AllArgsConstructor
+@NoArgsConstructor
+public class Cart {
+    private int id;
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private int groupId;
+
+    @Id
+    @GeneratedValue
+    @NotNull
+    @Column(name = "Id", unique = true)
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Column(name = "name")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Column(name = "description")
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Column(name = "price")
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    @Column(name = "group_id")
+    public int getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(int groupId) {
+        this.groupId = groupId;
+    }
+}
