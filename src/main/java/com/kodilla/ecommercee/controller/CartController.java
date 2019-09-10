3082d5f5768eb9a56ce5233acd6c25578d26087b
maciejgrzelec86 <maciej.kodilla.grzelec@gmail.com>
2019-09-10 23:13:04 +0200

    CartController full implementation

--- src/main/java/com/kodilla/ecommercee/controller/CartController.java
+++ src/main/java/com/kodilla/ecommercee/controller/CartController.java
@@ -1,13 +1,17 @@
 package com.kodilla.ecommercee.controller;
 
+import com.kodilla.ecommercee.controller.exceptions.CartNotFoundException;
+import com.kodilla.ecommercee.domain.Cart;
+import com.kodilla.ecommercee.domain.Order;
 import com.kodilla.ecommercee.dto.CartDto;
 import com.kodilla.ecommercee.domain.Product;
-import com.kodilla.ecommercee.repository.ProductRepository;
+import com.kodilla.ecommercee.dto.OrderDto;
+import com.kodilla.ecommercee.mapper.CartMapper;
+import com.kodilla.ecommercee.mapper.OrderMapper;
+import com.kodilla.ecommercee.service.CartService;
+import com.kodilla.ecommercee.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import static org.springframework.util.MimeTypeUtils.APPLICATION_JSON_VALUE;
 
@@ -18,23 +22,44 @@ import static org.springframework.util.MimeTypeUtils.APPLICATION_JSON_VALUE;
 public class CartController {
 
     @Autowired
-    private ProductRepository productRepository;
+    private CartService cartService;
+
+    @Autowired
+    private CartMapper cartMapper;
+
+    @Autowired
+    private OrderMapper orderMapper;
+
+    @Autowired
+    private OrderService orderService;
 
     @PostMapping(value = "createNewCart", consumes = APPLICATION_JSON_VALUE)
     public void createNewCart(@RequestBody CartDto cartDto) {
+        cartService.addCart(cartMapper.mapToCart(cartDto));
     }
+
     @GetMapping(value = "getProductsFromCart")
-    public List<Product> getProductsFromCart(@RequestParam("cartId") Long cartId) {
-        return new ArrayList<> ();
+    public CartDto getProductsFromCart(@RequestParam("cartId") Long cartId) {
+        return cartMapper.mapToCartDto(cartService.getCart(cartId).orElseThrow(CartNotFoundException::new));
     }
+
     @PostMapping(value = "addProductToCart", consumes = APPLICATION_JSON_VALUE)
     public void addProductToCart(@RequestParam ("cartId") Long cartId, @RequestParam("productId") Long productId) {
+        Cart cart = cartService.getCart(cartId).orElseThrow(CartNotFoundException::new);
+        Product product = cartService.findProduct(productId).orElseThrow(CartNotFoundException::new);
+        cart.getProducts().add(product);
     }
+
     @DeleteMapping(value = "deleteProductFromCart")
-    public void deleteProductFromCard(@RequestParam ("cartId") Long cartId, @RequestParam("productId") Long productId) { }
+    public void deleteProductFromCard(@RequestParam ("cartId") Long cartId, @RequestParam("productId") Long productId) {
+        Cart cart = cartService.getCart(cartId).orElseThrow(CartNotFoundException::new);
+        Product product = cartService.findProduct(productId).orElseThrow(CartNotFoundException::new);
+        cart.getProducts().remove(product);
+    }
     
     @PostMapping(value = "createOrder", consumes = APPLICATION_JSON_VALUE)
-    public void createOrder(@RequestParam ("cartId") Long cartId) {
+    public OrderDto createOrder(@RequestParam ("order") Order order) {
+        return orderMapper.mapToOrderDto(orderService.saveOrder(order));
     }
 }
 
---
+++ src/main/java/com/kodilla/ecommercee/service/CartService.java
@@ -0,0 +1,46 @@
+package com.kodilla.ecommercee.service;
+
+import com.kodilla.ecommercee.domain.Cart;
+import com.kodilla.ecommercee.domain.Product;
+import com.kodilla.ecommercee.repository.CartRepository;
+import com.kodilla.ecommercee.repository.ProductRepository;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional
+public class CartService {
+    private CartRepository cartRepository;
+    private ProductRepository productRepository;
+
+    public CartService(CartRepository cartRepository) {
+        this.cartRepository = cartRepository;
+    }
+
+    public Optional<Cart> getCart(Long id) {
+        return cartRepository.findById(id);
+    }
+
+    public List<Cart> getCarts() {
+        return cartRepository.findAll();
+    }
+
+    public Cart saveCart(Cart cart) {
+        return cartRepository.save(cart);
+    }
+
+    public void deleteCart(Long id) {
+        cartRepository.deleteById(id);
+    }
+
+    public Cart addCart(final Cart cart){
+        return cartRepository.save(cart);
+    }
+
+    public Optional<Product> findProduct(final Long id) {
+        return productRepository.findById(id);
+    }
+}
