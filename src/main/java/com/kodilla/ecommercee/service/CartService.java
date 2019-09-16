55e18ce447a874e9d29c5e86b6f8dbbd39b00a3f
maciejgrzelec86 <maciej.kodilla.grzelec@gmail.com>
2019-09-16 17:08:57 +0200

    changed CartController

--- src/main/java/com/kodilla/ecommercee/service/CartService.java
+++ src/main/java/com/kodilla/ecommercee/service/CartService.java
@@ -2,11 +2,14 @@ package com.kodilla.ecommercee.service;
 
 import com.kodilla.ecommercee.domain.Cart;
 import com.kodilla.ecommercee.domain.Product;
+import com.kodilla.ecommercee.domain.User;
 import com.kodilla.ecommercee.repository.CartRepository;
 import com.kodilla.ecommercee.repository.ProductRepository;
+import com.kodilla.ecommercee.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -15,6 +18,7 @@ import java.util.Optional;
 public class CartService {
     private CartRepository cartRepository;
     private ProductRepository productRepository;
+    private UserRepository userRepository;
 
     public CartService(CartRepository cartRepository) {
         this.cartRepository = cartRepository;
@@ -24,20 +28,10 @@ public class CartService {
         return cartRepository.findById(id);
     }
 
-    public List<Cart> getCarts() {
-        return cartRepository.findAll();
-    }
-
-    public Cart saveCart(Cart cart) {
-        return cartRepository.save(cart);
-    }
-
-    public void deleteCart(Long id) {
-        cartRepository.deleteById(id);
-    }
-
-    public Cart addCart(final Cart cart){
-        return cartRepository.save(cart);
+    public Cart createCart(final Long userId){
+        Cart cart = new Cart(1L, null, null, null, null, new ArrayList<>(), userRepository.findById(userId).orElse(null));
+        Cart cart2 = cartRepository.save(cart);
+        return cart2;
     }
 
     public Optional<Product> findProduct(final Long id) {
