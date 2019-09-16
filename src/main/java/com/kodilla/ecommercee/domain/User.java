3c805d25963817d73a7f50ab11a82f0e1329a66a
maciejgrzelec86 <maciej.kodilla.grzelec@gmail.com>
2019-09-16 12:32:37 +0200

    annotations

--- src/main/java/com/kodilla/ecommercee/domain/User.java
+++ src/main/java/com/kodilla/ecommercee/domain/User.java
@@ -1,5 +1,7 @@
 package com.kodilla.ecommercee.domain;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
